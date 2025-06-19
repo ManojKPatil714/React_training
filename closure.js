263bc2385528f984b8042531e30021b0c2c7ee64
MPatil17 <MPatil17@slb.com>
2025-06-19 16:19:51 +0530

    commit comments

--- closure.js
+++ closure.js
@@ -3,18 +3,18 @@ const outerFunction = () => {
     let counter = 0;
 
   const innerFunction = () => {
-    console.log(counter++);
-    console.log(outerVariable);
-    console.log('..................END..');
+    console.log("show counter first ", counter++);//show 0 
+    console.log("show outer variable ", outerVariable); // show 'I am from the outer function'
+    console.log('show ..inner function ................END..');// show '..inner function ................END..'
   };
   
   return innerFunction;
 }
 
 const innerFunction = outerFunction();
-innerFunction(); // 0, I am from the outer function
-innerFunction();
-innerFunction();
+innerFunction(); // show all console from inerFunction
+innerFunction(); // show all console from inerFunction
+innerFunction(); // show all console from inerFunction
 
 function makeMultiply(multiplier) {
 
@@ -24,8 +24,10 @@ function makeMultiply(multiplier) {
 }
 
 const double = makeMultiply(2);
-console.log(double(5)); // 10
+console.log("call makeMultiply function ", double(5)); 
 
 const triple = makeMultiply(30);
-console.log(triple(15)); // 150
+console.log("call makeMultiply function again ",triple(15)); // 150
 
+//Closure is a combination of function bundled together (enclosed) with references to its surrounding state (the lexical environment)
+// closure gives you access to an outer functions variable and functions from an inner function
