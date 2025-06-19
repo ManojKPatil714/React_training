56b5523e2b5243abca4b9e1ab8a9dbb3079df5fd
MPatil17 <MPatil17@slb.com>
2025-06-19 16:40:29 +0530

    update closure function

--- closure.js
+++ closure.js
@@ -1,33 +1,29 @@
-const outerFunction = () => {
-    let outerVariable = 'I am from the outer function';
-    let counter = 0;
-
-  const innerFunction = () => {
-    console.log("show counter first ", counter++);//show 0 
-    console.log("show outer variable ", outerVariable); // show 'I am from the outer function'
-    console.log('show ..inner function ................END..');// show '..inner function ................END..'
-  };
-  
-  return innerFunction;
-}
-
-const innerFunction = outerFunction();
-innerFunction(); // show all console from inerFunction
-innerFunction(); // show all console from inerFunction
-innerFunction(); // show all console from inerFunction
-
-function makeMultiply(multiplier) {
-
-    return function (number) {
-        return number * multiplier;
-    }
-}
-
-const double = makeMultiply(2);
-console.log("call makeMultiply function ", double(5)); // 10
-
-const triple = makeMultiply(30);
-console.log("call makeMultiply function again ",triple(15)); // 150
-
-//Closure is a combination of function bundled together (enclosed) with references to its surrounding state (the lexical environment)
+//Closure - Closure is a combination of function bundled together (enclosed) with references to its surrounding state (the lexical environment)
 // closure gives you access to an outer functions variable and functions from an inner function
+
+function makeCounter() {
+    let count = 0; // This variable is enclosed in the closure
+
+    return function() {
+        count += 1; // Accessing and modifying the outer function's variable
+        console.log(`Count is: ${count}`);
+        return count;
+    };
+    
+}
+
+const counter = makeCounter(); // Create a new counter instance
+console.log("Counter created. Now you can use it.");
+// Using the counter
+counter(); // Count is: 1
+counter(); // Count is: 2
+
+function makeMultiplier(Multiply) {
+    return function(number) {
+        return number * Multiply; // Accessing the outer function's variable 'factor'
+    };
+}
+const double = makeMultiplier(2); // Create a multiplier that doubles the number
+console.log(double(5)); // 10
+const triple = makeMultiplier(3); // Create a multiplier that triples the number
+console.log(triple(5)); // 15
